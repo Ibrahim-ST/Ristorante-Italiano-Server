d610bf19847f42399a84d66730f352d1f5748012
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-05-03 21:00:03 +0600

    updated data/ corrected info

--- index.js
+++ index.js
@@ -3,23 +3,39 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const chefData = require('./data/chefData.json');
-
+const chef = require('./data/chef.json');
+const recipe = require('./data/chefData.json');
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('ristorante italiano running');
 })
 
-app.get('/chefdata', (req, res) => {
-    res.send(chefData);
+app.get('/chef', (req, res) => {
+    res.send(chef);
 })
 
-app.get('/chefdata/:id', (req, res) => {
+app.get('/recipe', (req, res) => {
+    res.send(recipe)
+})
+
+app.get('/recipe/:id', (req, res) => {
     const id = req.params.id;
     console.log(id);
-    const selectedData = chefData.find(d=> d.id===id);
-    console.log(selectedData);
+    const selectedRecipe = recipe.find(r => r.id === id);
+    res.send(selectedRecipe);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+    if (id === 0) {
+        res.send(recipe);
+    }
+    else{
+        const selectedRecipe = recipe.filter(r=> parseInt(r.id)===id);
+        res.send(selectedRecipe);
+    }
 })
 
 app.listen(port, () => {
