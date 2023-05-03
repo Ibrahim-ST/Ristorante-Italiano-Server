94a71eaa6059dcf4af501f5da87af370f23bdea2
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-05-03 17:40:57 +0600

    Added chef data

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
-const app = express()
-const port = 5000;
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chefData = require('./data/chefData.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('ristorante italiano running');
+})
+
+app.get('/chefdata', (req, res) => {
+    res.send(chefData);
+})
+
+app.get('/chefdata/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedData = chefData.find(d=> d.id===id);
+    console.log(selectedData);
 })
 
 app.listen(port, () => {
